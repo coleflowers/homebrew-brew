348f97a0adc95846c365d190283b84a6f3de438a
Leilei <coleflowersma@gmail.com>
2020-09-04 10:18:58 +0800

    ☀️ replace

--- Formula/Proto2json.rb
+++ Formula/Proto2json.rb
@@ -2,7 +2,7 @@ class Proto2json < Formula
   desc "proto文件转json"
   homepage "https://github.com/coleflowers"
   url "https://github.com/thinkcn/proto2json/releases/download/Beta1.0.2/proto2json-Beta1.0.2-macos-brew.tar.gz"
-  sha256 "81d70deb9f67a8a9ffc51e2eb31e2b74b2b7d4d27d4beb26d693ed4ca7aebbe1"
+  sha256 "c0872bfc32ec63689b9046ef0adfdd5909e74497bd26d5d1451d4f26dd59a58a"
   version "Beta1.0.2"
   def install
     bin.install "proto2json"
